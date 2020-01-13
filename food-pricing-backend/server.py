b49030822672e43ab83a0749a5f5d0da41303dfd
tech-ken <39785207+tech-ken@users.noreply.github.com>
2020-01-13 03:49:24 +0100

    pending for testing in the pi
    please read the comments in line 14, 52 and 70~77 for testing in the pi and debugging.
    read line 83 and 84 for debugging too

--- food-pricing-backend/server.py
+++ food-pricing-backend/server.py
@@ -11,7 +11,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications import imagenet_utils
 from flask import Flask, send_file
-from pyrebase_utils import db
+from pyrebase_utils import db # If an error occurs in this line, delete it
 
 app = Flask(__name__)
 
@@ -49,7 +49,7 @@ def create_features(dataset):
 def mainRoute():
     return 'Hello!'
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['GET']) # you can delete methods=['GET'] if this line return an error
 def predict():
     id = str(uuid.uuid4())
     with picamera.PiCamera() as camera:
@@ -63,11 +63,29 @@ def predict():
 # # loding the ml model 
     categories = ['Bread','Dairy product','Dessert','Egg','Fried food','Meat','Noodles/Pasta','Rice','Seafood', 'Soup', 'Vegetable/Fruit']
     picture_image = glob.glob(path)
-    
     picture_features = create_features(picture_image)
-
     prediction = model.predict(picture_features)
     os.remove("/home/pi/backend/food-pricing-backend/data/%s%s" %(id,jpg) )
+
+## under this is the code which sends the json to the firebase. This code is under testing. They might return you an error.
+
+## three types of errors might occure. The first one is syntax error. For this error, there are two possible reasons. 
+## the first reason is purely, syntax error in line 80. The second one is the library (pyrebase) is not maintained properly and
+## therefor, the unknown reason of syntax error occurs. read (https://stackoverflow.com/questions/57333606/unknown-syntax-error-on-calling-pyrebase-in-python)
+## the alternative is to use another library (python-firebase) which @tech-ken don't know if it works properly or not due to that never used it before.
+
+## line 78 might return an error also due to syntax error, but this is more a python syntax issue.
+
+    label = categories[np.argmax(prediction)
+    data = {"id": "%s%s" %(id.jpg), "label": "%s" %(label), "": "wieght": "10", "price": "20", "iscorrect": "false"}
+    db.child("/predictions").push(data)
+    
+## for debugging perpose. comment out line 79 ~ 81 and activate line 84 and 85 to check if pyrebase for database works properly or not.
+## documents in (https://github.com/thisbejim/Pyrebase)
+                       
+#     fake = {"id": "a", "label": "b"}
+#     db.child("/predictions").push(fake)
+                       
     return "Predicted Class is: {}".format(categories[np.argmax(prediction)])
 
 if __name__ == '__main__':
