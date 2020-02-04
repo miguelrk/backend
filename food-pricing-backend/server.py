796794aca76355c75b3080f3525508d1979a4763
mertcanboz <52539978+mertcanboz@users.noreply.github.com>
2020-02-04 14:28:19 +0100

    Update server.py

--- food-pricing-backend/server.py
+++ food-pricing-backend/server.py
@@ -40,6 +40,20 @@ def predictRoute():
     print(data)
     return data
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=False)
 
