126fc8ea751d6abefa54ea8d87169db53f02351f
Mertcan Boz <mertboz82@gmail.com>
2020-02-06 20:06:09 +0100

    scale

--- food-pricing-backend/server.py
+++ food-pricing-backend/server.py
@@ -18,6 +18,25 @@ path = '/home/pi/backend/food-pricing-backend/data/*.jpg'
 jpg = '.jpg'
 imgpath = "/home/pi/backend/food-pricing-backend/"
 
+EMULATE_HX711=False
+referenceUnit = 1
+if not EMULATE_HX711:
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+else:
+    from emulated_hx711 import HX711
+def cleanAndExit():
+    print("Cleaning...")
+    if not EMULATE_HX711:
+        GPIO.cleanup()       
+    print("Bye!")
+    sys.exit()
+hx = HX711(5, 6)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(referenceUnit)
+hx.reset()
+hx.tare()
+
 @app.route('/')
 def mainRoute():
     return 'Hello!'
@@ -35,9 +54,10 @@ def predictRoute():
 
     #picture_image = glob.glob(path)
     #os.remove("/home/pi/backend/food-pricing-backend/data/%s%s" %(id,jpg) )
+    val = hx.get_weight(5)
 
     label = prediction
-    data = json.dumps({"id": id, "label": label, "weight": "10", "price": "20", "iscorrect": "false"})
+    data = json.dumps({"id": id, "label": label, "weight": "10", "price": "20", "iscorrect": "false", "weight":val })
     print(data)
     return data
 
