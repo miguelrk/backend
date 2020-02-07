3dbcf7044d460e85e3680031a8848c61e665c1dd
tech-ken <39785207+tech-ken@users.noreply.github.com>
2020-02-07 16:40:17 +0100

    working model updated

--- food-pricing-backend/visionModel.py
+++ food-pricing-backend/visionModel.py
@@ -2,9 +2,10 @@ import glob
 import numpy as np
 import os
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dropout, Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dropout, Dense, GlobalAveragePooling2D, Input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from netty import EfficientNetB0 as Net
+input_shape=(224, 224, 3)
 model_effinet = Net(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 model = Sequential()
 model.add(model_effinet)
@@ -12,11 +13,14 @@ model.add(GlobalAveragePooling2D())
 model.add(Dropout(0.35))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(100, activation='relu'))
-model.add(Dense(5, activation='softmax'))
-model.load_weights("model")
+model_input = Input(input_shape)
+model_evaluate = model(model_input)
+model_output = Dense(5, activation='softmax')(model_evaluate)
+evaluate_model = Model(inputs=model_input,outputs=model_output)
+evaluate_model.load_weights("model")
 categories = ['Jam Sandwich','Yoghurt','Banana','Lemon','Cookies']
 
-path = '/home/pi/backend/food-pricing-backend/picture.jfif' #this is for testing.
+path = '/home/pi/backend/food-pricing-backend/picture.jfif'
 sample_image = os.listdir()[0] #is the bread photo, this should be connected to the taken picture
 
 def create_features(imagePath):
@@ -31,6 +35,6 @@ def create_features(imagePath):
 
 def predict(imagePath = path):
     picture_features = create_features(imagePath)
-    prediction = model.predict(picture_features)
+    prediction = evaluate_model.predict(picture_features)
     return format(categories[np.argmax(prediction)])
 
